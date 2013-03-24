5105fc2764632ae6b7c6821d33d90102546f3083
Evan Solomon <evan@evanalyze.com>
2013-03-23 20:02:59 -0700

    Add the ability to set configs in ~/.scatterconfig
    Uses YAML

---
+++ lib/scatter/config.rb
@@ -0,0 +1,25 @@
+require 'yaml'
+
+module Scatter
+  class Config
+    CONFIG_FILE = "#{Dir.home}/.scatterconfig"
+    DEFAULTS = {'directory' => "#{Dir.home}/.deploys"}
+
+    def self.parse
+      return DEFAULTS unless File.exists? CONFIG_FILE
+
+      begin
+        return DEFAULTS.merge YAML.load(File.read CONFIG_FILE)
+      rescue
+        abort "There was a problem parsing #{CONFIG_FILE}, it should be valid YAML"
+      end
+    end
+
+    def self.get(key=nil)
+      config = self.parse
+      return config unless key
+
+      config.has_key?(key) ? config[key] : nil
+    end
+  end
+end
